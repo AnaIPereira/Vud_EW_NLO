0e7619220c8db69efc45a4c8162cb27d383ead4d
AnaIPereira <ana.pereira98@icloud.com>
2026-03-27 16:31:59 +0000

    attempt to fix wrong output QFTsymbols

--- 2loopampl/analyticalamplitude.m
+++ 2loopampl/analyticalamplitude.m
@@ -181,12 +181,9 @@ defoperq2[x_]:=Series[x/.{Ev3->Ev3 + (16 - a1q e)Op}/.{Ev5->Ev5 + (256-b1q e)Op 
 ];*)
 
 
-tomass[x_]:=x/.sw->Sqrt[1-cw^2]/.cw->MW/MZ
-
-
 (*GENERAL GAUGE*)
 ParallelTable[
-  diagram = Get[direc <> "/Results/2loop/gengauge/ampmastersmuonall/diag" <> ToString[i] <> ".m"]/.mass[x_]:>x;
+  diagram = Get[direc <> "/Results/2loop/gengauge/ampmastersmuonall/diag" <> ToString[i] <> ".m"]/.mass[x_]:>x/.sw->Sqrt[1-cw^2]/.cw->MW/MZ;
   diagram1 = tomass[diagram]/.d->4-2e/.mtad->sorttad/.rule;
   res = Normal[Series[diagram1, {e, 0, 0}]];
   (*res1 = res//Collect[#, {Op, Ev3, Ev5, 1/e, Log[__]}]&;*)
--- Results/2loop/gengauge/analytampsmuonrxi/diag451.m
+++
@@ -1,9 +0,0 @@
-(* Created with the Wolfram Language : www.wolfram.com *)
-((-6 + d)*el^6*Ev3*MZ^6*\[Xi]z*Flag[tadpolem]*
-   sorttad[{0, MZ*Sqrt[\[Xi]z], MW}, {0, 1, 1}])/
-  (2048*d*MH^2*MW^5*QFTSymbols`MW*(QFTSymbols`MW^2 - MZ^2)^2*Pi^8) - 
- ((-6 + d)*el^6*MZ^6*Op*\[Xi]z*Flag[tadpolem]*
-   ((-19 - \[Xi]A + d*(4 + \[Xi]A))*\[Xi]w*sorttad[{0, MZ*Sqrt[\[Xi]z], MW}, 
-      {0, 1, 1}] + (-1 + d + \[Xi]A)*sorttad[{0, MZ*Sqrt[\[Xi]z], 
-       MW*Sqrt[\[Xi]w]}, {0, 1, 1}]))/(2048*d*MH^2*MW^5*QFTSymbols`MW*
-   (QFTSymbols`MW^2 - MZ^2)^2*Pi^8*\[Xi]w)
