c4771ad5de8694fa4087cf04b5b2e550b6e35a70
AnaIPereira <ana.pereira98@icloud.com>
2026-03-27 18:15:38 +0000

    test

--- Results/2loop/gengauge/analytampsmuonrxi/diag22.m
+++
@@ -1,8 +0,0 @@
-(* Created with the Wolfram Language : www.wolfram.com *)
-((-2 + d)*el^6*Ev3*MZ^2*(-4 + d + \[Xi]A)*sorttad[{0, 0, MW}, {1, 1, 1}])/
-  (1024*d*MW^4*(QFTSymbols`MW^2 - MZ^2)*Pi^8) - 
- ((-2 + d)*el^6*MZ^2*Op*((76 + 4*d^2 - 19*\[Xi]A - \[Xi]A^2 + 
-      d*(-35 + 4*\[Xi]A + \[Xi]A^2))*\[Xi]w*sorttad[{0, 0, MW}, {1, 1, 1}] + 
-    (4 + d^2 + d*(-5 + \[Xi]A) - \[Xi]A + \[Xi]A^2)*
-     sorttad[{0, 0, MW*Sqrt[\[Xi]w]}, {1, 1, 1}]))/
-  (1024*d*MW^4*(QFTSymbols`MW^2 - MZ^2)*Pi^8*\[Xi]w)
--- Results/2loop/gengauge/analytampsmuonrxi/diag906.m
+++
@@ -1,7 +0,0 @@
-(* Created with the Wolfram Language : www.wolfram.com *)
-((-4 + d)*(-2 + d)*el^6*Ev3*MZ^4*nc*Flag[selfm]*NF[{MD}, {MU}]*
-   sorttad[{0, 0, MW}, {1, 1, 1}])/(1024*(-1 + d)*d*MW^4*
-   (QFTSymbols`MW^2 - MZ^2)^2*Pi^8) - 
- ((-4 + d)*(-2 + d)*el^6*MZ^4*nc*Op*(-19 - \[Xi]A + d*(4 + \[Xi]A))*
-   Flag[selfm]*NF[{MD}, {MU}]*sorttad[{0, 0, MW}, {1, 1, 1}])/
-  (1024*(-1 + d)*d*MW^4*(QFTSymbols`MW^2 - MZ^2)^2*Pi^8)
