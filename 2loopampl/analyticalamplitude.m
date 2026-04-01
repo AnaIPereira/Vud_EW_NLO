3de83c03894d2f30f40498e8b8f3ea58c782c2d0
AnaIPereira <ana.pereira98@icloud.com>
2026-04-01 12:33:17 +0100

    trial fix sorttad rules

--- 2loopampl/analyticalamplitude.m
+++ 2loopampl/analyticalamplitude.m
@@ -182,7 +182,7 @@ defoperq2[x_]:=Series[x/.{Ev3->Ev3 + (16 - a1q e)Op}/.{Ev5->Ev5 + (256-b1q e)Op 
 
 
 (*GENERAL GAUGE*)
-ParallelTable[
+Do[
   diagram = Get[direc <> "/Results/2loop/gengauge/ampmastersmuonall/diag" <> ToString[i] <> ".m"]/.mass[x_]:>x/.sw->Sqrt[1-cw^2]/.cw->MW/MZ;
   diagram1 = diagram/.d->4-2e/.mtad->sorttad/.rule;
   res1 = defoperm2[diagram1]//Collect[#, {Op, Ev3, Ev5, 1/e, Log[__], _PolyLog},Simplify]&;
--- Results/2loop/gengauge/analytampsmuonrxi/diag442.m
+++
@@ -1,7 +0,0 @@
-(* Created with the Wolfram Language : www.wolfram.com *)
-(-3*(-6 + d)*el^6*Ev3*MZ^4*Flag[tadpolem]*sorttad[{0, MH, MW}, {0, 1, 1}])/
-  (4096*d*MW^6*(QFTSymbols`MW^2 - MZ^2)^2*Pi^8) + 
- (3*(-6 + d)*el^6*MZ^4*Op*Flag[tadpolem]*
-   ((-19 - \[Xi]A + d*(4 + \[Xi]A))*\[Xi]w*sorttad[{0, MH, MW}, {0, 1, 1}] + 
-    (-1 + d + \[Xi]A)*sorttad[{0, MH, MW*Sqrt[\[Xi]w]}, {0, 1, 1}]))/
-  (4096*d*MW^6*(QFTSymbols`MW^2 - MZ^2)^2*Pi^8*\[Xi]w)
