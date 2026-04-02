4c1ec804f1f701896e12ce909de7272256d45124
AnaIPereira <ana.pereira98@icloud.com>
2026-04-02 11:17:14 +0100

    new trial

--- Results/2loop/gengauge/analytampsmuonrxi/diag904.m
+++
@@ -1,9 +0,0 @@
-(* Created with the Wolfram Language : www.wolfram.com *)
--1/1024*((2 - d)*el^6*Ev3*Flag[selfm]*NF[{MM}]*
-    (-4*sorttad[{0, 0, mass[MW]}, {1, 1, 1}] + 
-     d*sorttad[{0, 0, mass[MW]}, {1, 1, 1}]))/((-1 + d)*d*Pi^8*sw^4*
-    mass[MW]^4) - (el^6*Op*(38 - 27*d + 4*d^2 + 2*\[Xi]A - 3*d*\[Xi]A + 
-    d^2*\[Xi]A)*Flag[selfm]*NF[{MM}]*
-   (-4*sorttad[{0, 0, mass[MW]}, {1, 1, 1}] + 
-    d*sorttad[{0, 0, mass[MW]}, {1, 1, 1}]))/(1024*(-1 + d)*d*Pi^8*sw^4*
-   mass[MW]^4)
