a1ad6699be8c4a3fab5ed4d129057b0f8ee462ff
AnaIPereira <ana.pereira98@icloud.com>
2026-04-09 16:26:16 +0100

    change dir

--- 2loopampl/analyticalamplitude.m
+++ 2loopampl/analyticalamplitude.m
@@ -4,12 +4,12 @@
 (*set up directory*)
 
 
-(*to run locally*)
-direc=SetDirectory["/home/ana/Documents/GitHub/Vud_EW_NLO"];
+(*(*to run locally*)
+direc=SetDirectory["/home/ana/Documents/GitHub/Vud_EW_NLO"];*)
 
 
-(*(*to run in cluster*)
-direc=SetDirectory["/z/users/acpereira/Vud_EW_NLO"];*)
+(*to run in cluster*)
+direc=SetDirectory["/z/users/acpereira/Vud_EW_NLO"];
 
 
 (* ::Section::Closed:: *)
