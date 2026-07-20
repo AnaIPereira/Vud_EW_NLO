ad44fd168456596c0add8db015e727b9958f8e12
AnaIPereira <ana.pereira98@icloud.com>
2026-07-20 09:15:50 +0100

    Vud update

---
+++ CNLOfinal_noflags.m
@@ -0,0 +1,6 @@
+{ + Log[MZ] * ( rat[4,1] )
+
+       + Log[mu] * ( rat[-4,1] )
+
+       + a1qu*rat[1,12] + a1mu*rat[1,4] + rat[-5,1]}
+
---
+++ CNNLOfinal_noflags.m
@@ -0,0 +1,219 @@
+{ + Log[Mt]^2 * ( nc*rat[6*Mt^8*MW^4 + 6*Mt^8*MW^2*MZ^2 + 6*Mt^8*MZ^4 - 12*
+         Mt^6*MW^4*MZ^2 - 3*Mt^6*MW^2*MZ^4 - 9*Mt^2*MW^4*MZ^6 + 3*MW^6*MZ^6 - 
+         MW^4*MZ^8,9*Mt^2*MW^8*MZ^2 - 9*Mt^2*MW^6*MZ^4] )
+
+       + Log[Mt]*Log[MW] * ( nc*rat[4*Mt^8*MZ^4 - 2*Mt^6*MW^2*MZ^4 - 12*Mt^4*
+         MW^4*MZ^4 + 10*Mt^2*MW^6*MZ^4 - 2*MW^8*MZ^4,3*Mt^2*MW^10 - 6*Mt^2*
+         MW^8*MZ^2 + 3*Mt^2*MW^6*MZ^4] )
+
+       + Log[Mt]*Log[MZ] * ( nc*rat[ - 12*Mt^8*MW^2 + 24*Mt^6*MW^2*MZ^2 - 18*
+         Mt^6*MZ^4 + 36*Mt^4*MZ^6 - 12*Mt^2*MW^2*MZ^6 - 18*Mt^2*MZ^8 + 8*MW^2*
+         MZ^8 - 2*MZ^10,9*Mt^2*MW^6*MZ^2 - 18*Mt^2*MW^4*MZ^4 + 9*Mt^2*MW^2*
+         MZ^6] )
+
+       + Log[Mt] * ( nc*rat[ - 54*Mt^4*MH^2*MW^2*MZ^2 - 54*Mt^4*MH^2*MZ^4 - 
+         648*Mt^4*MW^2*MZ^4 + 1920*Mt^2*MH^2*MW^6 - 2208*Mt^2*MH^2*MW^4*MZ^2
+          + 477*Mt^2*MH^2*MW^2*MZ^4 + 320*MH^2*MW^6*MZ^2 - 320*MH^2*MW^4*MZ^4
+          - 18*MH^2*MW^2*MZ^6,81*MH^2*MW^6*MZ^2 - 81*MH^2*MW^4*MZ^4] )
+
+       + Log[MH]^2 * ( rat[18*MH^6 - 16*MH^4*MW^2, - 9*MH^2*MW^4 + 9*MW^6] )
+
+       + Log[MH]*Log[MW] * ( rat[MH^8*MW^4 + MH^8*MW^2*MZ^2 + MH^8*MZ^4 - 4*
+         MH^6*MW^6 - 7*MH^6*MW^4*MZ^2 + 5*MH^6*MW^2*MZ^4 - 3*MH^6*MZ^6 + 6*
+         MH^4*MW^8 + 21*MH^4*MW^6*MZ^2 - 12*MH^4*MW^4*MZ^4 + 18*MH^4*MW^2*MZ^6
+          + 3*MH^4*MZ^8 - 4*MH^2*MW^10 - 25*MH^2*MW^8*MZ^2 - MH^2*MW^6*MZ^4 - 
+         4*MH^2*MW^4*MZ^6 - 13*MH^2*MW^2*MZ^8 - MH^2*MZ^10 + MW^12 + 10*MW^10*
+         MZ^2 - 11*MW^8*MZ^4 - 11*MW^6*MZ^6 + 10*MW^4*MZ^8 + MW^2*MZ^10, - 3*
+         MH^2*MW^8*MZ^2 + 3*MH^2*MW^6*MZ^4 + 3*MW^10*MZ^2 - 3*MW^8*MZ^4] )
+
+       + Log[MH]*Log[MZ] * ( rat[ - 3*MH^8*MW^4 - 3*MH^8*MW^2*MZ^2 - 3*MH^8*
+         MZ^4 + 12*MH^6*MW^6 - 15*MH^6*MW^4*MZ^2 + 21*MH^6*MW^2*MZ^4 + 9*MH^6*
+         MZ^6 - 18*MH^4*MW^8 - 31*MH^4*MW^6*MZ^2 + 4*MH^4*MW^4*MZ^4 - 54*MH^4*
+         MW^2*MZ^6 - 9*MH^4*MZ^8 + 12*MH^2*MW^10 + 75*MH^2*MW^8*MZ^2 + 3*MH^2*
+         MW^6*MZ^4 + 12*MH^2*MW^4*MZ^6 + 39*MH^2*MW^2*MZ^8 + 3*MH^2*MZ^10 - 3*
+         MW^12 - 30*MW^10*MZ^2 + 33*MW^8*MZ^4 + 33*MW^6*MZ^6 - 30*MW^4*MZ^8 - 
+         3*MW^2*MZ^10, - 9*MH^2*MW^8*MZ^2 + 9*MH^2*MW^6*MZ^4 + 9*MW^10*MZ^2 - 
+         9*MW^8*MZ^4] )
+
+       + Log[MH] * ( rat[ - 18*MH^6*MW^2 + 24*MH^4*MW^4 - 9*MH^4*MW^2*MZ^2 - 6
+         *MH^4*MZ^4 - 6*MH^2*MW^6 - 36*MH^2*MW^4*MZ^2 + 6*MH^2*MW^2*MZ^4 - 16*
+         MW^4*MZ^4 + 16*MW^2*MZ^6, - 18*MH^2*MW^6 + 18*MH^2*MW^4*MZ^2 + 18*
+         MW^8 - 18*MW^6*MZ^2] )
+
+       + Log[MW]^2 * ( nc*rat[ - 2*Mt^8*MZ^4 + Mt^6*MW^2*MZ^4 + 6*Mt^4*MW^4*
+         MZ^4 - 5*Mt^2*MW^6*MZ^4 + MW^8*MZ^4,3*Mt^2*MW^10 - 6*Mt^2*MW^8*MZ^2
+          + 3*Mt^2*MW^6*MZ^4] + rat[ - 3*MH^8*MW^6*MZ^4 + 12*MH^6*MW^8*MZ^4 + 
+         9*MH^6*MW^6*MZ^6 - 36*MH^6*MW^4*MZ^8 + 18*MH^6*MW^2*MZ^10 - 18*MH^4*
+         MW^10*MZ^4 - 45*MH^4*MW^8*MZ^6 + 99*MH^4*MW^6*MZ^8 - 18*MH^4*MW^4*
+         MZ^10 + 2*MH^2*MW^16 - 19*MH^2*MW^14*MZ^2 + 93*MH^2*MW^12*MZ^4 - 112*
+         MH^2*MW^10*MZ^6 + 32*MH^2*MW^8*MZ^8 + 79*MH^2*MW^6*MZ^10 + 10*MH^2*
+         MW^4*MZ^12 - 12*MH^2*MW^2*MZ^14 + 8*MH^2*MZ^16 - 2*MW^18 + 19*MW^16*
+         MZ^2 - 84*MW^14*MZ^4 + 150*MW^12*MZ^6 - 45*MW^10*MZ^8 - 143*MW^8*
+         MZ^10 + 22*MW^6*MZ^12 - 16*MW^4*MZ^14, - 9*MH^2*MW^10*MZ^6 + 18*MH^2*
+         MW^8*MZ^8 - 9*MH^2*MW^6*MZ^10 + 9*MW^12*MZ^6 - 18*MW^10*MZ^8 + 9*MW^8
+         *MZ^10] )
+
+       + Log[MW]*Log[MZ] * ( rat[3*MH^8*MW^6*MZ^4 + 3*MH^8*MZ^10 - 12*MH^6*
+         MW^8*MZ^4 - 9*MH^6*MW^6*MZ^6 + 36*MH^6*MW^4*MZ^8 - 12*MH^6*MW^2*MZ^10
+          - 9*MH^6*MZ^12 + 18*MH^4*MW^10*MZ^4 + 45*MH^4*MW^8*MZ^6 - 99*MH^4*
+         MW^6*MZ^8 - 54*MH^4*MW^4*MZ^10 + 45*MH^4*MW^2*MZ^12 + 9*MH^4*MZ^14 - 
+         4*MH^2*MW^16 + 38*MH^2*MW^14*MZ^2 - 174*MH^2*MW^12*MZ^4 + 287*MH^2*
+         MW^10*MZ^6 - 136*MH^2*MW^8*MZ^8 - 149*MH^2*MW^6*MZ^10 + 7*MH^2*MW^4*
+         MZ^12 - 12*MH^2*MW^2*MZ^14 - 19*MH^2*MZ^16 + 4*MW^18 - 38*MW^16*MZ^2
+          + 165*MW^14*MZ^4 - 327*MW^12*MZ^6 + 153*MW^10*MZ^8 + 286*MW^8*MZ^10
+          - 107*MW^6*MZ^12 + 59*MW^4*MZ^14 + 3*MW^2*MZ^16, - 9*MH^2*MW^10*MZ^6
+          + 18*MH^2*MW^8*MZ^8 - 9*MH^2*MW^6*MZ^10 + 9*MW^12*MZ^6 - 18*MW^10*
+         MZ^8 + 9*MW^8*MZ^10] )
+
+       + Log[MW] * ( nc*rat[ - 2*Mt^4*MZ^4 + Mt^2*MW^2*MZ^4,3*MW^8 - 6*MW^6*
+         MZ^2 + 3*MW^4*MZ^4] + rat[6*MH^6*MW^6*MZ^2 + 6*MH^6*MW^2*MZ^6 - 12*
+         MH^6*MZ^8 + 1432*MH^4*MW^10 - 1434*MH^4*MW^8*MZ^2 - 1221*MH^4*MW^6*
+         MZ^4 + 1078*MH^4*MW^4*MZ^6 + 230*MH^4*MW^2*MZ^8 - 16*MH^4*MZ^10 - 
+         1432*MH^2*MW^12 + 1428*MH^2*MW^10*MZ^2 + 1050*MH^2*MW^8*MZ^4 - 897*
+         MH^2*MW^6*MZ^6 - 250*MH^2*MW^4*MZ^8 + 32*MH^2*MW^2*MZ^10 + 216*MW^10*
+         MZ^4 - 216*MW^8*MZ^6, - 18*MH^4*MW^8*MZ^2 + 36*MH^4*MW^6*MZ^4 - 18*
+         MH^4*MW^4*MZ^6 + 18*MH^2*MW^10*MZ^2 - 36*MH^2*MW^8*MZ^4 + 18*MH^2*
+         MW^6*MZ^6] )
+
+       + Log[MZ]^2 * ( nc*rat[18*Mt^8*MW^2 - 36*Mt^6*MW^2*MZ^2 + 27*Mt^6*MZ^4
+          - 54*Mt^4*MZ^6 + 176*Mt^2*MW^6*MZ^2 - 352*Mt^2*MW^4*MZ^4 + 194*Mt^2*
+         MW^2*MZ^6 + 27*Mt^2*MZ^8 - 12*MW^2*MZ^8 + 3*MZ^10,27*Mt^2*MW^6*MZ^2
+          - 54*Mt^2*MW^4*MZ^4 + 27*Mt^2*MW^2*MZ^6] + rat[ - 3*MH^8*MZ^10 + 18*
+         MH^6*MW^6*MZ^6 - 36*MH^6*MW^4*MZ^8 + 12*MH^6*MW^2*MZ^10 + 9*MH^6*
+         MZ^12 - 16*MH^4*MW^8*MZ^6 + 32*MH^4*MW^6*MZ^8 + 56*MH^4*MW^4*MZ^10 - 
+         45*MH^4*MW^2*MZ^12 - 9*MH^4*MZ^14 + 2*MH^2*MW^16 - 19*MH^2*MW^14*MZ^2
+          + 81*MH^2*MW^12*MZ^4 - 391*MH^2*MW^10*MZ^6 + 536*MH^2*MW^8*MZ^8 - 
+         146*MH^2*MW^6*MZ^10 - 17*MH^2*MW^4*MZ^12 + 24*MH^2*MW^2*MZ^14 + 11*
+         MH^2*MZ^16 - 2*MW^18 + 19*MW^16*MZ^2 - 81*MW^14*MZ^4 + 393*MW^12*MZ^6
+          - 540*MW^10*MZ^8 + 73*MW^8*MZ^10 + 85*MW^6*MZ^12 - 43*MW^4*MZ^14 - 3
+         *MW^2*MZ^16, - 9*MH^2*MW^10*MZ^6 + 18*MH^2*MW^8*MZ^8 - 9*MH^2*MW^6*
+         MZ^10 + 9*MW^12*MZ^6 - 18*MW^10*MZ^8 + 9*MW^8*MZ^10] )
+
+       + Log[MZ]*Log[mu] * ( nc*rat[-352,27] + rat[-48,1] )
+
+       + Log[MZ] * ( a1qu*rat[8*MW^4 - 8*MW^2*MZ^2 - 3*MZ^4,24*MW^4 - 24*MW^2*
+         MZ^2] + a1mu*rat[ - 8*MW^4 + 8*MW^2*MZ^2 + 3*MZ^4, - 8*MW^4 + 8*MW^2*
+         MZ^2] + nc*rat[18*Mt^4*MW^2*MZ^2 - 640*Mt^2*MW^6 + 1376*Mt^2*MW^4*
+         MZ^2 - 841*Mt^2*MW^2*MZ^4 + 96*Mt^2*MZ^6 - 640*MW^6*MZ^2 + 1304*MW^4*
+         MZ^4 - 916*MW^2*MZ^6 + 252*MZ^8,27*MW^6*MZ^2 - 54*MW^4*MZ^4 + 27*MW^2
+         *MZ^6] + nc*a1qu*rat[ - MZ^4, - 24*MW^4 + 24*MW^2*MZ^2] + nc*a1mu*
+         rat[ - MZ^4, - 8*MW^4 + 8*MW^2*MZ^2] + rat[ - 18*MH^6*MW^4*MZ^2 + 18*
+         MH^6*MW^2*MZ^4 + 12*MH^4*MW^6*MZ^2 + 39*MH^4*MW^4*MZ^4 - 45*MH^4*MW^2
+         *MZ^6 - 6*MH^4*MZ^8 + 1432*MH^2*MW^10 - 2052*MH^2*MW^8*MZ^2 + 546*
+         MH^2*MW^6*MZ^4 - 89*MH^2*MW^4*MZ^6 + 248*MH^2*MW^2*MZ^8 - 16*MH^2*
+         MZ^10 + 108*MW^4*MZ^8 - 108*MW^2*MZ^10,18*MH^2*MW^8*MZ^2 - 36*MH^2*
+         MW^6*MZ^4 + 18*MH^2*MW^4*MZ^6] )
+
+       + Log[mu]^2 * ( nc*rat[176,27] + rat[24,1] )
+
+       + Log[mu] * ( a1qu*rat[ - 8*MW^4 + 8*MW^2*MZ^2 + 3*MZ^4,24*MW^4 - 24*
+         MW^2*MZ^2] + a1mu*rat[ - 8*MW^4 + 8*MW^2*MZ^2 + 3*MZ^4,8*MW^4 - 8*
+         MW^2*MZ^2] + nc*rat[ - 648*Mt^4*MZ^2 + 162*Mt^2*MH^2*MZ^2 - 1600*MH^2
+         *MW^4 + 1672*MH^2*MW^2*MZ^2 - 774*MH^2*MZ^4, - 81*MH^2*MW^4 + 81*MH^2
+         *MW^2*MZ^2] + nc*a1qu*rat[MZ^4, - 24*MW^4 + 24*MW^2*MZ^2] + nc*a1mu*
+         rat[MZ^4, - 8*MW^4 + 8*MW^2*MZ^2] + rat[9*MH^4*MZ^2 - 104*MH^2*MW^4
+          + 198*MH^2*MW^2*MZ^2 - 5*MH^2*MZ^4 + 36*MW^4*MZ^2 + 18*MZ^6, - 3*
+         MH^2*MW^4 + 3*MH^2*MW^2*MZ^2] )
+
+       + Li2[1 - MW^(-2)*MZ^2] * ( rat[4*MH^2*MW^16 - 38*MH^2*MW^14*MZ^2 + 97*
+         MH^2*MW^12*MZ^4 - 114*MH^2*MW^10*MZ^6 + 10*MH^2*MW^8*MZ^8 + 74*MH^2*
+         MW^6*MZ^10 + 128*MH^2*MW^4*MZ^12 - 134*MH^2*MW^2*MZ^14 + 27*MH^2*
+         MZ^16 - 4*MW^18 + 38*MW^16*MZ^2 - 97*MW^14*MZ^4 + 118*MW^12*MZ^6 - 18
+         *MW^10*MZ^8 - 82*MW^8*MZ^10 - 96*MW^6*MZ^12 + 106*MW^4*MZ^14 - 19*
+         MW^2*MZ^16, - 18*MH^2*MW^10*MZ^6 + 36*MH^2*MW^8*MZ^8 - 18*MH^2*MW^6*
+         MZ^10 + 18*MW^12*MZ^6 - 36*MW^10*MZ^8 + 18*MW^8*MZ^10] )
+
+       + Li2[1 - MH^2*MW^(-2)] * ( rat[ - MH^8*MW^4 - MH^8*MW^2*MZ^2 - MH^8*
+         MZ^4 + 4*MH^6*MW^6 + 7*MH^6*MW^4*MZ^2 - 5*MH^6*MW^2*MZ^4 - 6*MH^4*
+         MW^8 - 21*MH^4*MW^6*MZ^2 + 12*MH^4*MW^4*MZ^4 + 4*MH^2*MW^10 + 25*MH^2
+         *MW^8*MZ^2 + MH^2*MW^6*MZ^4 - MW^12 - 10*MW^10*MZ^2 + 11*MW^8*MZ^4,
+          - 6*MH^2*MW^8*MZ^2 + 6*MH^2*MW^6*MZ^4 + 6*MW^10*MZ^2 - 6*MW^8*MZ^4]
+          )
+
+       + Li2[1 - MH^2*MZ^(-2)] * ( rat[ - 6*MH^10*MW^4 + 3*MH^10*MW^2*MZ^2 + 3
+         *MH^10*MZ^4 + 6*MH^8*MW^6 + 60*MH^8*MW^4*MZ^2 - 21*MH^8*MW^2*MZ^4 - 9
+         *MH^8*MZ^6 - 59*MH^6*MW^6*MZ^2 - 112*MH^6*MW^4*MZ^4 + 54*MH^6*MW^2*
+         MZ^6 + 9*MH^6*MZ^8 + 126*MH^4*MW^6*MZ^4 + 24*MH^4*MW^4*MZ^6 - 39*MH^4
+         *MW^2*MZ^8 - 3*MH^4*MZ^10 - 81*MH^2*MW^6*MZ^6 + 42*MH^2*MW^4*MZ^8 + 3
+         *MH^2*MW^2*MZ^10 + 8*MW^6*MZ^8 - 8*MW^4*MZ^10, - 18*MH^4*MW^8*MZ^2 + 
+         18*MH^4*MW^6*MZ^4 + 18*MH^2*MW^10*MZ^2 - 18*MH^2*MW^8*MZ^4] )
+
+       + Li2[1 - Mt^2*MW^(-2)] * ( nc*rat[ - 2*Mt^8*MZ^4 + Mt^6*MW^2*MZ^4 + 6*
+         Mt^4*MW^4*MZ^4 - 7*Mt^2*MW^6*MZ^4 + 2*MW^8*MZ^4,6*Mt^2*MW^10 - 12*
+         Mt^2*MW^8*MZ^2 + 6*Mt^2*MW^6*MZ^4] )
+
+       + Li2[1 - Mt^2*MZ^(-2)] * ( nc*rat[2*Mt^8*MW^2 - 4*Mt^6*MW^2*MZ^2 + 3*
+         Mt^6*MZ^4 - 6*Mt^4*MZ^6 + 4*Mt^2*MW^2*MZ^6 + 3*Mt^2*MZ^8 - 2*MW^2*
+         MZ^8,6*Mt^2*MW^6*MZ^2 - 12*Mt^2*MW^4*MZ^4 + 6*Mt^2*MW^2*MZ^6] )
+
+       + DTPHI1[1/4*Mt^(-2)*MZ^2] * ( nc*rat[ - 1280*Mt^6*MW^6 + 2752*Mt^6*
+         MW^4*MZ^2 - 1628*Mt^6*MW^2*MZ^4 + 84*Mt^6*MZ^6 - 128*Mt^4*MW^6*MZ^2
+          + 256*Mt^4*MW^4*MZ^4 - 188*Mt^4*MW^2*MZ^6 + 186*Mt^4*MZ^8 + 64*Mt^2*
+         MW^6*MZ^4 - 128*Mt^2*MW^4*MZ^6 + 22*Mt^2*MW^2*MZ^8 - 21*Mt^2*MZ^10 + 
+         12*MW^2*MZ^10 - 3*MZ^12,108*Mt^2*MW^6*MZ^4 - 216*Mt^2*MW^4*MZ^6 + 108
+         *Mt^2*MW^2*MZ^8] )
+
+       + DTPHI1[1/4*Mt^(-2)*MW^2] * ( nc*rat[8*Mt^6*MZ^4 - 14*Mt^4*MW^2*MZ^4
+          + 7*Mt^2*MW^4*MZ^4 - MW^6*MZ^4,12*Mt^2*MW^8 - 24*Mt^2*MW^6*MZ^2 + 12
+         *Mt^2*MW^4*MZ^4] )
+
+       + DTPHI1[1/4*MW^(-2)*MZ^2] * ( rat[1440*MW^14 - 1080*MW^12*MZ^2 - 1908*
+         MW^10*MZ^4 + 1406*MW^8*MZ^6 - 269*MW^6*MZ^8 - 324*MW^4*MZ^10 + 160*
+         MW^2*MZ^12 - 19*MZ^14,36*MW^10*MZ^4 - 72*MW^8*MZ^6 + 36*MW^6*MZ^8] )
+
+       + DTPHI1[1/4*MW^2*MZ^(-2)] * ( rat[ - 2*MH^2*MW^14 + 23*MH^2*MW^12*MZ^2
+          - 115*MH^2*MW^10*MZ^4 + 307*MH^2*MW^8*MZ^6 - 348*MH^2*MW^6*MZ^8 + 70
+         *MH^2*MW^4*MZ^10 - 296*MH^2*MW^2*MZ^12 + 64*MH^2*MZ^14 + 2*MW^16 - 23
+         *MW^14*MZ^2 + 115*MW^12*MZ^4 - 309*MW^10*MZ^6 + 356*MW^8*MZ^8 - 64*
+         MW^6*MZ^10 + 268*MW^4*MZ^12 - 48*MW^2*MZ^14, - 36*MH^2*MW^8*MZ^6 + 72
+         *MH^2*MW^6*MZ^8 - 36*MH^2*MW^4*MZ^10 + 36*MW^10*MZ^6 - 72*MW^8*MZ^8
+          + 36*MW^6*MZ^10] )
+
+       + DTPHI1[1/4*MH^2*MW^(-2)] * ( rat[ - MH^6*MZ^4 + 2*MH^4*MW^2*MZ^4 + 8*
+         MH^2*MW^4*MZ^4,12*MW^10 - 24*MW^8*MZ^2 + 12*MW^6*MZ^4] )
+
+       + DTPHI1[1/4*MH^2*MZ^(-2)] * ( rat[ - 6*MH^8*MW^2 + 9*MH^8*MZ^2 + 6*
+         MH^6*MW^4 + 48*MH^6*MW^2*MZ^2 - 63*MH^6*MZ^4 - 55*MH^4*MW^4*MZ^2 - 
+         145*MH^4*MW^2*MZ^4 + 182*MH^4*MZ^6 + 200*MH^2*MW^4*MZ^4 + 80*MH^2*
+         MW^2*MZ^6 - 256*MH^2*MZ^8 - 268*MW^4*MZ^6 + 284*MW^2*MZ^8 - 16*MZ^10,
+          - 36*MH^2*MW^6*MZ^2 + 72*MH^2*MW^4*MZ^4 - 36*MH^2*MW^2*MZ^6 + 36*
+         MW^8*MZ^2 - 72*MW^6*MZ^4 + 36*MW^4*MZ^6] )
+
+       + DTPHI2[MH^2*MW^(-2),MW^(-2)*MZ^2] * ( rat[MH^8*MW^6 + MH^8*MZ^6 - 4*
+         MH^6*MW^8 - 4*MH^6*MW^6*MZ^2 + 12*MH^6*MW^4*MZ^4 - 4*MH^6*MW^2*MZ^6
+          - 4*MH^6*MZ^8 + 6*MH^4*MW^10 + 16*MH^4*MW^8*MZ^2 - 30*MH^4*MW^6*MZ^4
+          - 30*MH^4*MW^4*MZ^6 + 16*MH^4*MW^2*MZ^8 + 6*MH^4*MZ^10 - 4*MH^2*
+         MW^12 - 20*MH^2*MW^10*MZ^2 + 16*MH^2*MW^8*MZ^4 + 16*MH^2*MW^6*MZ^6 + 
+         16*MH^2*MW^4*MZ^8 - 20*MH^2*MW^2*MZ^10 - 4*MH^2*MZ^12 + MW^14 + 8*
+         MW^12*MZ^2 - 30*MW^10*MZ^4 + 21*MW^8*MZ^6 + 21*MW^6*MZ^8 - 30*MW^4*
+         MZ^10 + 8*MW^2*MZ^12 + MZ^14,12*MW^12*MZ^2 - 24*MW^10*MZ^4 + 12*MW^8*
+         MZ^6] )
+
+       + pi^2*rat[ - 18*MH^6*MW^4*MZ^6 + 18*MH^6*MW^2*MZ^8 - 2*MH^4*MW^6*MZ^6
+       + 2*MH^4*MW^4*MZ^8 + 2*MH^2*MW^14 - 17*MH^2*MW^12*MZ^2 - MH^2*MW^10*
+      MZ^4 + 154*MH^2*MW^8*MZ^6 - 140*MH^2*MW^6*MZ^8 + 22*MH^2*MW^4*MZ^10 - 4*
+      MH^2*MW^2*MZ^12 + 8*MH^2*MZ^14 - 8*MW^4*MZ^12 + 8*MW^2*MZ^14,108*MH^2*
+      MW^8*MZ^6 - 108*MH^2*MW^6*MZ^8] + b2qu*rat[-1,288] + b1qu*rat[7,192] + 
+         b2mu*rat[1,32] + b1mu*rat[-5,64] + a2qu*rat[ - 80*MW^4 + 80*MW^2*MZ^2
+          - 9*MZ^4, - 288*MW^4 + 288*MW^2*MZ^2] + a1qu*rat[ - 1784*MW^4 + 1784
+         *MW^2*MZ^2 + 297*MZ^4,1728*MW^4 - 1728*MW^2*MZ^2] + a2mu*rat[16*MW^4
+          - 16*MW^2*MZ^2 - 3*MZ^4, - 32*MW^4 + 32*MW^2*MZ^2] + a1mu*rat[ - 152
+         *MW^4 + 152*MW^2*MZ^2 - 99*MZ^4, - 192*MW^4 + 192*MW^2*MZ^2] + a1mu*
+         a1qu*rat[1,48] + a1mu^2*rat[1,16] + S2*rat[297*MZ^4,4*MW^4 - 8*MW^2*
+         MZ^2 + 4*MZ^4] + nc*rat[324*Mt^4*MH^2*MW^2*MZ^2 + 324*Mt^4*MH^2*MZ^4
+          - 3888*Mt^4*MW^2*MZ^4 - 23040*Mt^2*MH^2*MW^6 + 26496*Mt^2*MH^2*MW^4*
+         MZ^2 - 3375*Mt^2*MH^2*MW^2*MZ^4 - 17164*MH^2*MW^6*MZ^2 + 17884*MH^2*
+         MW^4*MZ^4 - 8361*MH^2*MW^2*MZ^6, - 972*MH^2*MW^6*MZ^2 + 972*MH^2*MW^4
+         *MZ^4] + nc*pi^2*rat[18*Mt^8*MW^4 + 18*Mt^8*MW^2*MZ^2 + 18*Mt^8*MZ^4
+          - 36*Mt^6*MW^4*MZ^2 - 9*Mt^6*MW^2*MZ^4 + 176*Mt^2*MW^8*MZ^2 - 176*
+         Mt^2*MW^6*MZ^4 - 9*MW^6*MZ^6 - 3*MW^4*MZ^8,324*Mt^2*MW^8*MZ^2 - 324*
+         Mt^2*MW^6*MZ^4] + nc*a2qu*rat[ - 176*MW^4 + 176*MW^2*MZ^2 + 27*MZ^4,
+          - 2592*MW^4 + 2592*MW^2*MZ^2] + nc*a1qu*rat[176*MW^4 - 176*MW^2*MZ^2
+          + 891*MZ^4, - 15552*MW^4 + 15552*MW^2*MZ^2] + nc*a2mu*rat[ - 176*
+         MW^4 + 176*MW^2*MZ^2 + 27*MZ^4, - 864*MW^4 + 864*MW^2*MZ^2] + nc*a1mu
+         *rat[176*MW^4 - 176*MW^2*MZ^2 + 891*MZ^4, - 5184*MW^4 + 5184*MW^2*
+         MZ^2] + rat[36*MH^6*MW^2*MZ^2 - 24*MH^4*MW^4*MZ^2 - 261*MH^4*MW^2*
+         MZ^4 + 24*MH^4*MZ^6 - 5752*MH^2*MW^8 + 1232*MH^2*MW^6*MZ^2 + 1484*
+         MH^2*MW^4*MZ^4 - 255*MH^2*MW^2*MZ^6 + 32*MH^2*MZ^8 - 144*MW^6*MZ^4 - 
+         72*MW^2*MZ^8,72*MH^2*MW^6*MZ^2 - 72*MH^2*MW^4*MZ^4]}
+
